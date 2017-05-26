d7ebfc749ef214a545638223198dc8b65f8f60b3
avandehoef <avandehoef@outlook.com>
2017-05-26 22:56:04 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Vlaggen
+{
+    partial class vlag
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(vlag));
+            this.btnVolgendeVlag = new System.Windows.Forms.Button();
+            this.btnStartOpnieuw = new System.Windows.Forms.Button();
+            this.btnAlleVlaggen = new System.Windows.Forms.Button();
+            this.btnAntwoordB = new System.Windows.Forms.Button();
+            this.btnAntwoordA = new System.Windows.Forms.Button();
+            this.btnAntwoordD = new System.Windows.Forms.Button();
+            this.btnAntwoordC = new System.Windows.Forms.Button();
+            this.textOpeningsTekst = new System.Windows.Forms.TextBox();
+            this.textScoreAlleenText = new System.Windows.Forms.TextBox();
+            this.textScoreOutput = new System.Windows.Forms.TextBox();
+            this.textBijAntwoordA = new System.Windows.Forms.TextBox();
+            this.textBijAntwoordB = new System.Windows.Forms.TextBox();
+            this.textBijAntwoordC = new System.Windows.Forms.TextBox();
+            this.textBijAntwoordD = new System.Windows.Forms.TextBox();
+            this.textAntwoordCorrect = new System.Windows.Forms.TextBox();
+            this.textAntwoordInCorrect = new System.Windows.Forms.TextBox();
+            this.pictureVlag = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVlag)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVolgendeVlag
+            // 
+            this.btnVolgendeVlag.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolgendeVlag.Location = new System.Drawing.Point(526, 505);
+            this.btnVolgendeVlag.Name = "btnVolgendeVlag";
+            this.btnVolgendeVlag.Size = new System.Drawing.Size(155, 66);
+            this.btnVolgendeVlag.TabIndex = 1;
+            this.btnVolgendeVlag.Text = "Volgende vlag";
+            this.btnVolgendeVlag.UseVisualStyleBackColor = true;
+            this.btnVolgendeVlag.Click += new System.EventHandler(this.btnVolgendeVlag_Click);
+            // 
+            // btnStartOpnieuw
+            // 
+            this.btnStartOpnieuw.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartOpnieuw.Location = new System.Drawing.Point(526, 629);
+            this.btnStartOpnieuw.Name = "btnStartOpnieuw";
+            this.btnStartOpnieuw.Size = new System.Drawing.Size(155, 66);
+            this.btnStartOpnieuw.TabIndex = 2;
+            this.btnStartOpnieuw.Text = "Start / opnieuw";
+            this.btnStartOpnieuw.UseVisualStyleBackColor = true;
+            this.btnStartOpnieuw.Click += new System.EventHandler(this.btnStartOpnieuw_Click);
+            // 
+            // btnAlleVlaggen
+            // 
+            this.btnAlleVlaggen.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlleVlaggen.Location = new System.Drawing.Point(717, 629);
+            this.btnAlleVlaggen.Name = "btnAlleVlaggen";
+            this.btnAlleVlaggen.Size = new System.Drawing.Size(155, 66);
+            this.btnAlleVlaggen.TabIndex = 3;
+            this.btnAlleVlaggen.Text = "Bekijk alle vlaggen";
+            this.btnAlleVlaggen.UseVisualStyleBackColor = true;
+            this.btnAlleVlaggen.Click += new System.EventHandler(this.btnAlleVlaggen_Click);
+            // 
+            // btnAntwoordB
+            // 
+            this.btnAntwoordB.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAntwoordB.Location = new System.Drawing.Point(59, 262);
+            this.btnAntwoordB.Name = "btnAntwoordB";
+            this.btnAntwoordB.Size = new System.Drawing.Size(140, 46);
+            this.btnAntwoordB.TabIndex = 5;
+            this.btnAntwoordB.Text = "B";
+            this.btnAntwoordB.UseVisualStyleBackColor = true;
+            this.btnAntwoordB.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAntwoordA
+            // 
+            this.btnAntwoordA.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAntwoordA.Location = new System.Drawing.Point(59, 177);
+            this.btnAntwoordA.Name = "btnAntwoordA";
+            this.btnAntwoordA.Size = new System.Drawing.Size(140, 46);
+            this.btnAntwoordA.TabIndex = 4;
+            this.btnAntwoordA.Text = "A";
+            this.btnAntwoordA.UseVisualStyleBackColor = true;
+            this.btnAntwoordA.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAntwoordD
+            // 
+            this.btnAntwoordD.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAntwoordD.Location = new System.Drawing.Point(59, 429);
+            this.btnAntwoordD.Name = "btnAntwoordD";
+            this.btnAntwoordD.Size = new System.Drawing.Size(140, 46);
+            this.btnAntwoordD.TabIndex = 7;
+            this.btnAntwoordD.Text = "D";
+            this.btnAntwoordD.UseVisualStyleBackColor = true;
+            this.btnAntwoordD.Click += new System.EventHandler(this.btnAntwoordD_Click);
+            // 
+            // btnAntwoordC
+            // 
+            this.btnAntwoordC.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAntwoordC.Location = new System.Drawing.Point(59, 344);
+            this.btnAntwoordC.Name = "btnAntwoordC";
+            this.btnAntwoordC.Size = new System.Drawing.Size(140, 46);
+            this.btnAntwoordC.TabIndex = 6;
+            this.btnAntwoordC.Text = "C";
+            this.btnAntwoordC.UseVisualStyleBackColor = true;
+            this.btnAntwoordC.Click += new System.EventHandler(this.btnAntwoordC_Click);
+            // 
+            // textOpeningsTekst
+            // 
+            this.textOpeningsTekst.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textOpeningsTekst.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold);
+            this.textOpeningsTekst.Location = new System.Drawing.Point(59, 22);
+            this.textOpeningsTekst.Name = "textOpeningsTekst";
+            this.textOpeningsTekst.ReadOnly = true;
+            this.textOpeningsTekst.Size = new System.Drawing.Size(813, 35);
+            this.textOpeningsTekst.TabIndex = 8;
+            this.textOpeningsTekst.Text = "Combineer de vlaggen met het juiste land!";
+            this.textOpeningsTekst.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textScoreAlleenText
+            // 
+            this.textScoreAlleenText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textScoreAlleenText.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textScoreAlleenText.Location = new System.Drawing.Point(526, 105);
+            this.textScoreAlleenText.Name = "textScoreAlleenText";
+            this.textScoreAlleenText.ReadOnly = true;
+            this.textScoreAlleenText.Size = new System.Drawing.Size(181, 25);
+            this.textScoreAlleenText.TabIndex = 9;
+            this.textScoreAlleenText.Text = "SCORE";
+            this.textScoreAlleenText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textScoreOutput
+            // 
+            this.textScoreOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textScoreOutput.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textScoreOutput.Location = new System.Drawing.Point(526, 136);
+            this.textScoreOutput.Name = "textScoreOutput";
+            this.textScoreOutput.ReadOnly = true;
+            this.textScoreOutput.Size = new System.Drawing.Size(181, 25);
+            this.textScoreOutput.TabIndex = 10;
+            this.textScoreOutput.Text = "0";
+            this.textScoreOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBijAntwoordA
+            // 
+            this.textBijAntwoordA.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBijAntwoordA.Location = new System.Drawing.Point(228, 185);
+            this.textBijAntwoordA.Name = "textBijAntwoordA";
+            this.textBijAntwoordA.ReadOnly = true;
+            this.textBijAntwoordA.Size = new System.Drawing.Size(262, 32);
+            this.textBijAntwoordA.TabIndex = 11;
+            this.textBijAntwoordA.Text = "Verenigde Arabische Emiraten";
+            this.textBijAntwoordA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBijAntwoordB
+            // 
+            this.textBijAntwoordB.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBijAntwoordB.Location = new System.Drawing.Point(228, 270);
+            this.textBijAntwoordB.Name = "textBijAntwoordB";
+            this.textBijAntwoordB.ReadOnly = true;
+            this.textBijAntwoordB.Size = new System.Drawing.Size(262, 32);
+            this.textBijAntwoordB.TabIndex = 12;
+            this.textBijAntwoordB.Text = "Verenigde Arabische Emiraten";
+            this.textBijAntwoordB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBijAntwoordC
+            // 
+            this.textBijAntwoordC.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBijAntwoordC.Location = new System.Drawing.Point(228, 352);
+            this.textBijAntwoordC.Name = "textBijAntwoordC";
+            this.textBijAntwoordC.ReadOnly = true;
+            this.textBijAntwoordC.Size = new System.Drawing.Size(262, 32);
+            this.textBijAntwoordC.TabIndex = 13;
+            this.textBijAntwoordC.Text = "Verenigde Arabische Emiraten";
+            this.textBijAntwoordC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBijAntwoordD
+            // 
+            this.textBijAntwoordD.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBijAntwoordD.Location = new System.Drawing.Point(228, 438);
+            this.textBijAntwoordD.Name = "textBijAntwoordD";
+            this.textBijAntwoordD.ReadOnly = true;
+            this.textBijAntwoordD.Size = new System.Drawing.Size(262, 32);
+            this.textBijAntwoordD.TabIndex = 14;
+            this.textBijAntwoordD.Text = "Verenigde Arabische Emiraten";
+            this.textBijAntwoordD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textAntwoordCorrect
+            // 
+            this.textAntwoordCorrect.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textAntwoordCorrect.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAntwoordCorrect.ForeColor = System.Drawing.Color.Green;
+            this.textAntwoordCorrect.Location = new System.Drawing.Point(778, 277);
+            this.textAntwoordCorrect.Name = "textAntwoordCorrect";
+            this.textAntwoordCorrect.ReadOnly = true;
+            this.textAntwoordCorrect.Size = new System.Drawing.Size(181, 25);
+            this.textAntwoordCorrect.TabIndex = 15;
+            this.textAntwoordCorrect.Text = "Goed!";
+            this.textAntwoordCorrect.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textAntwoordInCorrect
+            // 
+            this.textAntwoordInCorrect.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textAntwoordInCorrect.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAntwoordInCorrect.ForeColor = System.Drawing.Color.Red;
+            this.textAntwoordInCorrect.Location = new System.Drawing.Point(778, 338);
+            this.textAntwoordInCorrect.Name = "textAntwoordInCorrect";
+            this.textAntwoordInCorrect.ReadOnly = true;
+            this.textAntwoordInCorrect.Size = new System.Drawing.Size(181, 25);
+            this.textAntwoordInCorrect.TabIndex = 16;
+            this.textAntwoordInCorrect.Text = "Niet goed";
+            this.textAntwoordInCorrect.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureVlag
+            // 
+            this.pictureVlag.Image = global::Vlaggen.Properties.Resources.Vlag0;
+            this.pictureVlag.Location = new System.Drawing.Point(526, 234);
+            this.pictureVlag.Name = "pictureVlag";
+            this.pictureVlag.Size = new System.Drawing.Size(249, 193);
+            this.pictureVlag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureVlag.TabIndex = 0;
+            this.pictureVlag.TabStop = false;
+            this.pictureVlag.Click += new System.EventHandler(this.pictureVlag_Click);
+            // 
+            // vlag
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 721);
+            this.Controls.Add(this.textAntwoordInCorrect);
+            this.Controls.Add(this.textAntwoordCorrect);
+            this.Controls.Add(this.textBijAntwoordD);
+            this.Controls.Add(this.textBijAntwoordC);
+            this.Controls.Add(this.textBijAntwoordB);
+            this.Controls.Add(this.textBijAntwoordA);
+            this.Controls.Add(this.textScoreOutput);
+            this.Controls.Add(this.textScoreAlleenText);
+            this.Controls.Add(this.textOpeningsTekst);
+            this.Controls.Add(this.btnAntwoordD);
+            this.Controls.Add(this.btnAntwoordC);
+            this.Controls.Add(this.btnAntwoordB);
+            this.Controls.Add(this.btnAntwoordA);
+            this.Controls.Add(this.btnAlleVlaggen);
+            this.Controls.Add(this.btnStartOpnieuw);
+            this.Controls.Add(this.btnVolgendeVlag);
+            this.Controls.Add(this.pictureVlag);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "vlag";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vlaggen van de Wereld";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVlag)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureVlag;
+        private System.Windows.Forms.Button btnVolgendeVlag;
+        private System.Windows.Forms.Button btnStartOpnieuw;
+        private System.Windows.Forms.Button btnAlleVlaggen;
+        private System.Windows.Forms.Button btnAntwoordB;
+        private System.Windows.Forms.Button btnAntwoordA;
+        private System.Windows.Forms.Button btnAntwoordD;
+        private System.Windows.Forms.Button btnAntwoordC;
+        private System.Windows.Forms.TextBox textOpeningsTekst;
+        private System.Windows.Forms.TextBox textScoreAlleenText;
+        private System.Windows.Forms.TextBox textScoreOutput;
+        private System.Windows.Forms.TextBox textBijAntwoordA;
+        private System.Windows.Forms.TextBox textBijAntwoordB;
+        private System.Windows.Forms.TextBox textBijAntwoordC;
+        private System.Windows.Forms.TextBox textBijAntwoordD;
+        private System.Windows.Forms.TextBox textAntwoordCorrect;
+        private System.Windows.Forms.TextBox textAntwoordInCorrect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Reflection;
+
+namespace Vlaggen
+{
+    public partial class vlag : Form
+    {
+        
+            
+
+    public vlag()
+        {
+            InitializeComponent();
+        }
+
+
+        Image[] imagesVlaggenArray = new Image[197];
+        int ScoreOutput = 0;
+
+        Random randomNummer1 = new Random();
+        Random randomNummer2 = new Random();
+        Random randomNummer3 = new Random();
+        Random randomNummer4 = new Random();
+        Random randomNummer5 = new Random();
+
+        private void pictureVlag_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnStartOpnieuw_Click(object sender, EventArgs e)
+        {
+            int randomVlag = randomNummer1.Next(1, 197);
+            int corresponderendeRandomLand = randomVlag;
+            pictureVlag.Image = (Image)Properties.Resources.ResourceManager.GetObject("Vlag" + randomVlag);
+
+            List<string> landen = File.ReadAllLines(@"InfoLanden.txt").ToList();
+            var ding = landen.ElementAt(corresponderendeRandomLand);
+            textBijAntwoordA.Text = Convert.ToString(ding);
+            ScoreOutput = 0;
+            textScoreOutput.Text = Convert.ToString(ScoreOutput);
+        }
+
+        private void btnAntwoordC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAntwoordD_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVolgendeVlag_Click(object sender, EventArgs e)
+        {                       
+            int randomVlag = randomNummer1.Next(1, 197);
+            int corresponderendeRandomLand = randomVlag;
+            pictureVlag.Image = (Image)Properties.Resources.ResourceManager.GetObject("Vlag" + randomVlag);
+
+            List<string> landen = File.ReadAllLines(@"InfoLanden.txt").ToList();
+            var ding = landen.ElementAt(corresponderendeRandomLand);
+            textBijAntwoordA.Text = Convert.ToString(ding);
+            ScoreOutput = ScoreOutput + 1;
+            textScoreOutput.Text = Convert.ToString(ScoreOutput);
+
+        }
+
+        private void btnAlleVlaggen_Click(object sender, EventArgs e)
+        {
+            alleVlaggen alleVlaggen = new alleVlaggen();
+            alleVlaggen.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
